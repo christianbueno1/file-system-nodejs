85d747b2560962ff7eafd91c672cd31ef79e85e8
miwi <miwi@adobe.com>
2020-10-24 21:48:26 -0500

    add numbers1 list comprehension

--- hello.py
+++ hello.py
@@ -1,7 +1,8 @@
 print("hello my friends")
-for i in range(13):
+for i in range(7):
     print(i)
-
+numbers1 = [1,2,3,4,5,6,7,8,9,10]
+print([i for i in numbers1 if i%2 == 0])
 fruits = ['kiwi','apple','grape','banana','lima']
 for f in fruits:
     print(f,end=', ')
